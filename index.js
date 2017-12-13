096c4c3db4d145b5c0b6ae429414166d07ebcb92
Ryan-McBride <mcbrideryan89@gmail.com>
2017-12-13 09:00:25 -0800

    removing connect and end

--- index.js
+++ index.js
@@ -16,14 +16,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/shuffle', function(req, res) {
-  connection.connect();
   connection.query('SELECT * FROM gunpla ORDER BY rand() LIMIT 2', function (error, results, fields) {
     if(error){
       fs.appendFileSync('kitbash.log', error+'\n');
     }
     res.send(JSON.stringify(results));
   });
-  connection.end();
 });
 
 app.get('/page-not-found', function(req, res){
